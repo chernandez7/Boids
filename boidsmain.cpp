d909cb41248ad3c8b5799572eab8b47685ce8b83
Jorge Yanar <jorgeyanar@gmail.com>
2015-04-17 11:35:34 -0500

    Added general structure for program. This includes the relevant classes and their header files: the Boids class, the Flock class, which instantiates a flock of boids, and main.

---
+++ boidsmain.cpp
@@ -0,0 +1,16 @@
+// This file acts as the main for our boids project. Here, we utilize the SFML
+// library, import boids and flock classes, and run the program.
+#include <iostream>
+#include <vector>
+// Include: Boids
+//          Flock?
+//          SFML
+using namespace std;
+
+int main()
+{
+    // Initialize variables -- perhaps create a flock object, and create 
+    // window. 
+    
+    return 0;
+}
