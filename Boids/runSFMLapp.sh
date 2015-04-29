81bb1ee0ec6431f09107e4d23de7236c9934e268
Jorge Yanar <jorgeyanar@gmail.com>
2015-04-28 22:25:10 -0500

    The "makefile" runSFMLapp.sh now deletes *h.gch files in the directory, which was causing compiling errors.

--- Boids/runSFMLapp.sh
+++ Boids/runSFMLapp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-g++ main.cpp Boid.cpp Boid.h Pvector.cpp Pvector.h
+rm -f *.o sfml-app *h.gch
+g++ -c main.cpp Boid.cpp Pvector.cpp Flock.cpp
 # Below, compile the program and append the necessary libraries
-#g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+g++ main.o Boid.o Pvector.o Flock.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
 # Run the executable
-#./sfml-app
+./sfml-app
